1bb39a0217bf707fbf7625ebcf622eed81b7ddb0
Kevin Harrington <harrington@wpi.edu>
2019-07-26 15:42:13 -0400

    added base class bits
    For Mike to reference later.

--- JumpScript.groovy
+++ JumpScript.groovy
@@ -1,1 +1,19 @@
 //Your code here
+
+if(args==null){
+	args=[]
+	args[0]=DeviceManager.getSpecificDevice( "MediumKat",{return null})
+}
+def cat = args[0]
+println cat
+class Jumper{
+	private myInternalCat
+	public Jumper(def myCat){
+		myInternalCat=myCat
+	}
+	def jump(){
+		println "Hello world"
+	}
+}
+
+return new Jumper(cat)
